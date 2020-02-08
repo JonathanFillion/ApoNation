d8ce8326f43e1cf89e2be3a3a000fe6299a434e6
Jonathan Fillion <fillion.jonathan@gmail.com>
2020-02-07 22:50:16 -0500

    Backup before Vehicule

--- Assets/Scripts/VehiculeController.cs
+++ Assets/Scripts/VehiculeController.cs
@@ -1,77 +1,60 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
-[RequireComponent(typeof(CharacterController))]
-
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
 
 public class VehiculeController : MonoBehaviour
 {
+    public List<AxleInfo> axleInfos;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
 
-    public float speed = 7.5f;
-    public float jumpSpeed = 8.0f;
-    public float gravity = 20.0f;
-    public Transform playerCameraParent;
-    public float lookSpeed = 2.0f;
-    public float lookXLimit = 60.0f;
-
-    CharacterController characterController;
-    Vector3 moveDirection = new Vector3(0, 0, 0);
-    Vector2 rotation = new Vector2(0, 0);
-
-    [HideInInspector]
-    public bool canMove = true;
-
-    void Start()
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
-        characterController = GetComponent<CharacterController>();
-
-    }
-
-
-    void Update()
-    {
-        if (PresentEntitiesManager.instance.isVehiculeControl)
+        if (collider.transform.childCount == 0)
         {
-            CheckForVehiculeExit();
-            if (characterController.isGrounded)
-            {
-                //Normalized vectors responsible for direction
-                Vector3 forward = transform.TransformDirection(Vector3.forward);
-                Vector3 right = transform.TransformDirection(Vector3.right);
-                //Input keys detection
-                float currentSpeedX = canMove ? speed * Input.GetAxis("Vertical") : 0;
-                //Store current move detection with predefined speed on moveVector
-                moveDirection = (forward * currentSpeedX);
-
-                if (Input.GetButton("Jump") && canMove)
-                {
-                    moveDirection.y = jumpSpeed;
-                }
-            }
-            //Applying gravity
-            moveDirection.y -= gravity * Time.deltaTime;
-            //https://forum.unity.com/threads/adding-gravity-unity-c-basic-code.359966/
-            characterController.Move(moveDirection * Time.deltaTime);
-            transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
-            //Camera rotation
-
-            if (canMove)
-            {
-                rotation.y += Input.GetAxis("Mouse X") * lookSpeed;
-                rotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
-                rotation.x = Mathf.Clamp(rotation.x, -lookXLimit, lookXLimit);
-                playerCameraParent.localRotation = Quaternion.Euler(rotation.x, rotation.y, 0);
-                //transform.eulerAngles = new Vector2(0, rotation.y);
-            }
+            return;
         }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
     }
-    void CheckForVehiculeExit()
+
+    public void FixedUpdate()
     {
-        if (Input.GetKeyDown("e"))
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
         {
-            CameraManager.instance.SwitchView();
-            PresentEntitiesManager.instance.PlayerEntityEnabled();
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
     }
 }

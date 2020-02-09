c25cee922163d96fffa76073bf1a88138c22b488
Jonathan Fillion <fillion.jonathan@gmail.com>
2020-02-08 23:42:21 -0500

    Brake is being applied correctly

--- Assets/Scripts/VehiculeController.cs
+++ Assets/Scripts/VehiculeController.cs
@@ -16,9 +16,23 @@ public class VehiculeController : MonoBehaviour
     public List<AxleInfo> axleInfos;
     public float maxMotorTorque;
     public float maxSteeringAngle;
+    public float maxBraqueTorque;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 60.0f;
+    public Transform playerCameraParent;
 
-    // finds the corresponding visual wheel
-    // correctly applies the transform
+    private Vector2 cameraRotation = new Vector2(0, 0);
+    private Rigidbody rb;
+    private bool highone = false;
+    private bool lowone = false;
+    private float previousSpeed;
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        previousSpeed = 0;
+    }
+
     public void ApplyLocalPositionToVisuals(WheelCollider collider)
     {
         if (collider.transform.childCount == 0)
@@ -38,23 +52,47 @@ public class VehiculeController : MonoBehaviour
 
     public void FixedUpdate()
     {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
-        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+        float speed = rb.velocity.magnitude;
+        var velocity = rb.velocity;
+        var localVel = transform.InverseTransformDirection(velocity);
+        float motor = Mathf.Clamp(Input.GetAxis("Vertical"), -1, 1);
+        float steering = Mathf.Clamp(Input.GetAxis("Horizontal"), -1, 1);
 
+        float brake = 0;
+        if (localVel.z > 0)
+        {
+            brake = maxBraqueTorque * -1 * Mathf.Clamp(Input.GetAxis("Vertical"), -1, 0);
+        }
+        else
+        {
+            brake = maxBraqueTorque * Mathf.Clamp(Input.GetAxis("Vertical"), 0, 1);
+        }
+
+       
         foreach (AxleInfo axleInfo in axleInfos)
         {
             if (axleInfo.steering)
             {
-                axleInfo.leftWheel.steerAngle = steering;
-                axleInfo.rightWheel.steerAngle = steering;
+                axleInfo.leftWheel.steerAngle = steering * maxSteeringAngle;
+                axleInfo.rightWheel.steerAngle = steering * maxSteeringAngle; ;
             }
             if (axleInfo.motor)
             {
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
+                axleInfo.leftWheel.motorTorque = maxMotorTorque * motor;
+                axleInfo.rightWheel.motorTorque = maxMotorTorque * motor;
             }
+            
+
             ApplyLocalPositionToVisuals(axleInfo.leftWheel);
             ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
+
+        rb.drag = brake;
+
+        //Camera Rotation
+        cameraRotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+        cameraRotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        cameraRotation.x = Mathf.Clamp(cameraRotation.x, -lookXLimit, lookXLimit);
+        playerCameraParent.localRotation = Quaternion.Euler(cameraRotation.x, cameraRotation.y, 0);
     }
 }

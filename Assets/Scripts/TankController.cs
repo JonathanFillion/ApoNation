c93670ce3235cb142d79a78cd933b7d629d67c2e
Jonathan Fillion <fillion.jonathan@gmail.com>
2020-02-17 23:51:13 -0500

    Tank basic controls almost done git add .!

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -5,18 +5,23 @@ using System.Collections.Generic;
 
 public class TankController : MonoBehaviour
 {
-    public float accelaration = 10.0f;
+    public float accelarationFactor = 100.0f;
     public float maximumSpeed = 10.0f;
-    public float maximumTurningSpeed = 1.0f;
-    public float angularAcceleration = 10.0f;
+    public float maximumTurningSpeed = 100.0f;
+    public float rotationSpeed = 240.0f;
+    public float frictionBasicFactor = 4.0f;
     public float lookSpeed = 2.0f;
-    public float lookXLimit = 60.0f;
     public float turnDragging = 10.0f;
     public Transform cameraParent;
     public float DistanceToTheGround;
 
     private Vector2 cameraRotation = new Vector2(0, -90);
+    private float lookXLimitMax = 15.0f;
+    private float lookXLimitMin = 1.0f;
     private Rigidbody rb;
+    private Transform turret;
+    private Transform canon;
+    private Camera camera;
 
     //Reusability
     private bool isGrounded;
@@ -27,9 +32,68 @@ public class TankController : MonoBehaviour
     {
         DistanceToTheGround = GetComponent<Collider>().bounds.extents.y;
         rb = GetComponent<Rigidbody>();
+        turret = transform.Find("Tank Head Container");
+        canon = turret.Find("Tank Canon Container");
+        Cursor.lockState = CursorLockMode.Locked;
+        camera = GameObject.FindWithTag("TankCamera").GetComponent<Camera>();
+
     }
 
     public void Update()
+    {
+
+
+
+        /*Camera Rotation*/
+        cameraRotation.y += Input.GetAxis("Mouse X") * lookSpeed;
+        cameraRotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
+        cameraRotation.x = Mathf.Clamp(cameraRotation.x, lookXLimitMin, lookXLimitMax);
+        //cameraRotation.y = Mathf.Clamp(cameraRotation.y, lookYLimitMin, lookYLimitMax);
+        cameraParent.localRotation = Quaternion.Euler(0, cameraRotation.y, cameraRotation.x);
+
+        /*Frontal point related to tank body*/
+        float aimingDistanceTankBody = 100.0f;
+        RaycastHit frontalOfTank;
+        Vector3 frontalPoint = new Vector3();
+        Ray tankBodyRay = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+        if (Physics.Raycast(tankBodyRay, out frontalOfTank))
+        {
+            frontalPoint = frontalOfTank.point;
+            //Debug.DrawLine(transform.position, frontalOfTank.point, Color.red);
+        }
+        else
+        {
+            frontalPoint = tankBodyRay.origin + tankBodyRay.direction * aimingDistanceTankBody;
+            //Debug.DrawLine(tankBodyRay.origin, frontalPoint, Color.red);
+        }
+
+        /*Canon Rotation*/
+        RaycastHit hit;
+        float aimingDistance = 100.0f;
+        Vector3 aimPoint = new Vector3();
+        Vector3 canonAimPoint = new Vector3();
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit))
+        {
+            aimPoint = hit.point;
+            Debug.DrawLine(ray.origin, hit.point, Color.red);
+        }
+        else
+        {
+            aimPoint = ray.origin + ray.direction * aimingDistance;
+            Debug.DrawLine(ray.origin, aimPoint, Color.red);
+        }
+        canonAimPoint = aimPoint;
+        aimPoint.y = transform.position.y;
+        turret.LookAt(aimPoint);
+
+        print(camera.transform.position.y);
+        //4.05 a 8.51
+
+    }
+
+
+    void OnGUI()
     {
 
     }
@@ -39,7 +103,7 @@ public class TankController : MonoBehaviour
 
         float speed = rb.velocity.magnitude;
         var velocity = rb.velocity;
-        var localVel = transform.InverseTransformDirection(velocity);
+        var localVelocity = transform.InverseTransformDirection(velocity);
         var velocityAngle = Vector3.Angle(rb.velocity, transform.TransformDirection(Vector3.forward)) - 90;
         bool notorque = true;
         float traction = 1 / speed;
@@ -49,24 +113,23 @@ public class TankController : MonoBehaviour
         //print(DistanceToTheGround);
         //print(isGrounded);
 
+        /*Vertical*/
         if (vin != 0 && isGrounded == true)
         {
             notorque = false;
-            if (localVel.z > -maximumSpeed && localVel.z < maximumSpeed)
+            if (localVelocity.z > -maximumSpeed && localVelocity.z < maximumSpeed)
             {
-                rb.AddForce(transform.TransformDirection(Vector3.forward) * vin * accelaration * rb.mass);
+                rb.AddForce(transform.TransformDirection(Vector3.forward) * vin * accelarationFactor * rb.mass);
             }
         }
 
-
-
-
+        /*Rotation*/
         if (hin != 0 && isGrounded == true)
         {
             notorque = false;
             if (rb.angularVelocity.magnitude < maximumTurningSpeed)
             {
-                rb.AddTorque(Vector3.up * hin * angularAcceleration * rb.mass);
+                rb.AddTorque(Vector3.up * hin * rotationSpeed * rb.mass);
             }
         }
         else
@@ -74,6 +137,25 @@ public class TankController : MonoBehaviour
             rb.AddTorque(-rb.angularVelocity * turnDragging * rb.mass);
         }
 
+        /*Friction*/
+        float friction = 0f;
+        var frictionAngle = Vector3.Angle(localVelocity, -Vector3.forward);
+        if (frictionAngle < 90)
+        {
+            friction = frictionAngle;
+        }
+        else if (frictionAngle > 90)
+        {
+            friction = Mathf.Abs(frictionAngle - 180);
+        }
+        if (rb.velocity.magnitude >= 0.5 && isGrounded == true)
+        {
+            Vector3 negativeNormalizedVelocity = -rb.velocity.normalized;
+            rb.AddForce(negativeNormalizedVelocity * friction * frictionBasicFactor);
+        }
+
+        /*Slerp*/
+        /* https://docs.unity3d.com/ScriptReference/Vector3.Slerp.html */
         if (velocityAngle > 0 && notorque == false && isGrounded == true)
         {
             rb.velocity = Vector3.Slerp(rb.velocity, -transform.forward * rb.velocity.magnitude, traction);
@@ -82,15 +164,15 @@ public class TankController : MonoBehaviour
         {
             rb.velocity = Vector3.Slerp(rb.velocity, transform.forward * rb.velocity.magnitude, traction);
         }
-        //Camera Rotation
-        /*cameraRotation.y += Input.GetAxis("Mouse X") * lookSpeed;
-        cameraRotation.x += -Input.GetAxis("Mouse Y") * lookSpeed;
-        cameraRotation.x = Mathf.Clamp(cameraRotation.x, -lookXLimit, lookXLimit);
-        cameraParent.localRotation = Quaternion.Euler(0, cameraRotation.y, cameraRotation.x);*/
+
+
+
+
 
     }
 
-    void OnCollisionEnter(Collision collision) {
+    void OnCollisionEnter(Collision collision)
+    {
         isGrounded = true;
     }
 
@@ -99,11 +181,4 @@ public class TankController : MonoBehaviour
         isGrounded = false;
     }
 
-
-    void CheckForVehiculeExit()
-    {
-        if (Input.GetKeyDown("e") && !PresentEntitiesManager.instance.exitActionBlocked)
-        {
-        }
-    }
 }
